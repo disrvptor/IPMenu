71c3f626806df57e1dc79bbdf9634ee9544ece6e
Guy Pascarella <guy.pascarella@issinc.com>
2015-09-08 08:54:28 -0400

    Initial commit

---
+++ IP Menu-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  IP Menu-Bridging-Header.h
+//  IP Menu
+//
+//  Created by Guy Pascarella on 9/2/15.
+//
+//
+
+#ifndef IP_Menu_IP_Menu_Bridging_Header_h
+#define IP_Menu_IP_Menu_Bridging_Header_h
+
+#include <ifaddrs.h>
+
+#endif
